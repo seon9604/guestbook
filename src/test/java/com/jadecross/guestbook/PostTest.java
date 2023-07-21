8560afeccecbc3a78dc79cbff70f0b8ef494daf7
seon9604 <seon9604@gmail.com>
2023-07-21 16:23:46 +0900

    PostTest add

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class PostTest {
   @Test
   void testSetName() {
-    Post post = new Post("TESTER", "2022-04-12 01:01:48", "Happy Wedding");
+    Post post = new Post("TESTER", "2023-05-27 12:30:00", "Happy Wedding");
     post.setName("DEVOPS");
     assertEquals("DEVOPS", post.getName());
   }
